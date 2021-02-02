8531d5b9a4e9f65209389113fb5bb8996e7e8c50
Karan Verma <kv57@duke.edu>
2021-02-02 06:24:36 -0500

    Frontend modelpage changes readded

--- FiveGuysPowerTesting/urls.py
+++ FiveGuysPowerTesting/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from front import views as v
 
 urlpatterns = [
     path('models/', include('database.urls')),
     path('admin/', admin.site.urls),
+    path('', v.modelpage)
 ]
