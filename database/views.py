a308a16beaf7b9515192044c5318103e3e7acfa4
Januario Carreiro <jjc70@duke.edu>
2021-02-09 00:58:48 -0500

    Added query capability:

--- database/views.py
+++ database/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from rest_framework import permissions
 
+from django_filters.rest_framework import DjangoFilterBackend
+
 from database.models import EquipmentModel, Instrument, CalibrationEvent
 from database.serializers import EquipmentModelSerializer, InstrumentSerializer, CalibrationEventSerializer
 
@@ -12,6 +14,19 @@ class EquipmentModelViewSet(viewsets.ModelViewSet):
     queryset = EquipmentModel.objects.all()
     serializer_class = EquipmentModelSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['vendor']
+
+
+# class EquipmentModelFilterList(generics.ListAPIView):
+#     serializer_class = EquipmentModelSerializer
+#
+#     def get_queryset(self):
+#         queryset = EquipmentModel.objects.all()
+#         vendor = self.request.query_params.get('vendor', None)
+#         if vendor is not None:
+#             queryset = queryset.filter(equipmentmodel__vendor=vendor)
+#         return queryset
 
 
 class InstrumentViewSet(viewsets.ModelViewSet):
