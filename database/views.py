175ca3cb664b82c5fa1dca36997f058a1ba93290
Daniel Kingsbury <dpk14@duke.edu>
2021-02-09 11:23:13 -0500

    gitignore

--- database/views.py
+++ database/views.py
@@ -1,9 +1,6 @@
-from django.db.models import Model, Q
-from rest_framework import viewsets, generics
+from django.db.models import Q
 from rest_framework import permissions
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from rest_framework.serializers import ListSerializer
+from rest_framework import viewsets, generics
 
 from database.models import EquipmentModel, Instrument, CalibrationEvent
 from database.serializers import EquipmentModelSerializer, InstrumentSerializer, CalibrationEventSerializer, \
