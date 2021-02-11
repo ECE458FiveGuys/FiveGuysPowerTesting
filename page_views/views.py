47d0ff3690e0c1a3770f61169b9fe69157497be2
Karan Verma <kv57@duke.edu>
2021-02-11 18:33:12 -0500

    Reorganizing Search bars, Testing model data access

--- page_views/views.py
+++ page_views/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_list_or_404
+from database.models import Model
+from database.models import Instrument
+#from django.view import generic
 # Create your views here.
 def modelpage(request):
-    return render(request, 'modelpage.html')
+    modlist = get_list_or_404(Model)
+    return render(request, 'modelpage.html', {'modlist':modlist})
 
 def instrumentpage(request):
-    return render(request, 'instrumentpage.html')
+    instrlist = get_list_or_404(Instrument)
+    return render(request, 'instrumentpage.html', {'instrlist':instrlist})
+
