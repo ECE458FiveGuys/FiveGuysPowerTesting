33d9942f832c2c2762a0b3d097da17a95687bad9
Karan Verma <kv57@duke.edu>
2021-02-12 18:37:14 -0500

    Table pagination

--- page_views/views.py
+++ page_views/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render, get_list_or_404
-from database.models import EquipmentModel
-from database.models import Instrument
-#from django.view import generic
+import database as db
+from django.core.paginator import Paginator
+
+# from django.view import generic
 # Create your views here.
 def modelpage(request):
-    modlist = get_list_or_404(EquipmentModel)
-    return render(request, 'modelpage.html', {'modlist':modlist})
+    modlist = get_list_or_404(db.EquipmentModelViewSet.queryset)
+    paginator = Paginator(modlist, 25)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page('page_number')
+    return render(request, 'modelpage.html', {'modlist': page_obj})
+
 
 def instrumentpage(request):
-    instrlist = get_list_or_404(Instrument)
-    return render(request, 'instrumentpage.html', {'instrlist':instrlist})
-
+    instrlist = get_list_or_404(db.InstrumentViewSet.queryset)
+    paginator = Paginator(instrlist, 25)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page('page_number')
+    return render(request, 'instrumentpage.html', {'instrlist': page_obj})
