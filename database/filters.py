8fc8dee73dff023f5616563afa3bed39d1078c59
Januario Carreiro <jjc70@duke.edu>
2021-02-15 23:20:23 -0500

    Documentation for custom search.

--- database/filters.py
+++ database/filters.py
@@ -7,10 +7,14 @@ from rest_framework import filters
 
 
 class CustomSearchFilter(filters.SearchFilter):
+    """
+    Implementation of search that allows for keyword search on one specific field as opposed to all of the fields listed
+    in the search_fields variable of a ModelView.
+    """
     def get_search_fields(self, view, request):
-        search_fields = super(CustomSearchFilter, self).get_search_fields(view, request)
-        if "search_field" in request.query_params:
-            field_to_search = request.query_params.get("search_field")
-            if field_to_search in search_fields:
-                return [field_to_search]
+        search_fields = super(CustomSearchFilter, self).get_search_fields(view, request)  # get possible search fields
+        if "search_field" in request.query_params:  # check if doing a search on single field
+            field = request.query_params.get("search_field")  # get field attempting to search on
+            if field in search_fields:  # check if field is valid
+                return [field]
         return search_fields
