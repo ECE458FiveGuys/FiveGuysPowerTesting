b7b7c358878b3dfe12ca7cd0ea32b9dc535354ca
Januario Carreiro <jjc70@duke.edu>
2021-02-24 11:46:22 -0500

    Added is_staff field to /users/me/

--- user_portal/views.py
+++ user_portal/views.py
@@ -3,13 +3,16 @@ from rest_framework import permissions, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from user_portal.models import PowerUser
+from database.model_enums import UserEnum
 
 
 class ExtendedUserViewSet(viewsets.ModelViewSet):
     queryset = PowerUser.objects.all()
     serializer_class = serializers.UserSerializer
     permission_classes = [permissions.IsAuthenticated]
-    filter_backends = []
+    filterset_fields = [UserEnum.USERNAME.value]
+    search_fields = [UserEnum.USERNAME.value]
+    ordering_fields = [UserEnum.USERNAME.value]
 
     @action(detail=True, methods=['get'])
     def deactivate(self, request, pk):
