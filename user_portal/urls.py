698ae91b2c38d4a8ebd9778ebb1ec7d31f24e065
Januario Carreiro <jjc70@duke.edu>
2021-03-02 22:09:11 -0500

    Fixed bug where method /post/ not allowed on endpoint

--- user_portal/urls.py
+++ user_portal/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, re_path
 from rest_framework import routers
 
-from user_portal.views import ExtendedUserViewSet
+from user_portal.views import ExtendedUserViewSet, OAuthView
+
 router = routers.DefaultRouter()
 router.register(r'users', ExtendedUserViewSet)
 
@@ -9,4 +10,5 @@ urlpatterns = (
     re_path('', include("djoser.urls.base")),
     re_path('', include(router.urls)),
     re_path('', include("djoser.urls.authtoken")),
+    re_path('oauth/login/', OAuthView.as_view())
 )
