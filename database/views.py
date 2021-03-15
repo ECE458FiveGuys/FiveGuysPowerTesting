287486ef186fd24445041ddb5168d6f38d944f68
Januario Carreiro <jjc70@duke.edu>
2021-03-15 12:59:24 -0400

    Increased max_page_size to 1000

--- database/views.py
+++ database/views.py
@@ -27,7 +27,8 @@ from database.services.table_enums import MaxInstrumentTableColumnNames, MinInst
 class SmallResultsSetPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
-    max_page_size = 100
+    max_page_size = 1000
+
 
 class CategoryViewSet(viewsets.ModelViewSet):
     filterset_fields = [CategoryEnum.NAME.value]
