a358c05da81e58ff9f897a9f48de9608341334b4
Januario Carreiro <jjc70@duke.edu>
2021-03-18 01:35:51 -0400

    Minor bug-fixes for deployment

--- FiveGuysPowerTesting/settings.py
+++ FiveGuysPowerTesting/settings.py
@@ -82,16 +82,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'FiveGuysPowerTesting.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissions',
--- user_portal/signals.py
+++
@@ -1,6 +0,0 @@
-from django.dispatch import receiver
-from djoser.signals import user_activated
-
-# @receiver(user_activated)
-# def add_to_groups(user, request):
-#     if user.is_admin:
