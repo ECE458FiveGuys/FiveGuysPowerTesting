e130098a740af246e3a0101fe1e3e7fcfb6a413e
Karan Verma <kv57@duke.edu>
2021-03-26 05:19:33 -0400

    ssh command set initial

--- klufe/urls.py
+++ klufe/urls.py
@@ -2,5 +2,8 @@ from django.urls import re_path
 from klufe import views
 
 urlpatterns = [
-    re_path('', views.setDC),
+    re_path('/on', views.voltageOn),
+    re_path('/off', views.voltageOff),
+    re_path('/set/AC', views.setACVoltage),
+    re_path('/set/DC', views.setDCVoltage),
 ]
--- klufe/views.py
+++ klufe/views.py
@@ -8,7 +8,7 @@ import time
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
-def setDC(request):
+def voltageOn(request):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
     client.connect('hpt-k5700.colab.duke.edu', port=2222, username='admin6', password='foxY458')
@@ -16,6 +16,77 @@ def setDC(request):
 
     conn.recv(10000)
     conn.send('on\n')
+
+    n = 0
+    while n < 3:
+        time.sleep(0.5)
+        if conn.recv_ready():
+            break
+
+    prompt = conn.recv(10000).decode('ascii')
+
+    client.close()
+    return Response({re.split(r'\x1B[\[0-9;]*m', prompt)[2]})
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def voltageOff(request):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+    client.connect('hpt-k5700.colab.duke.edu', port=2222, username='admin6', password='foxY458')
+    conn = client.invoke_shell()
+
+    conn.recv(10000)
+    conn.send('off\n')
+
+    n = 0
+    while n < 3:
+        time.sleep(0.5)
+        if conn.recv_ready():
+            break
+
+    prompt = conn.recv(10000).decode('ascii')
+
+    client.close()
+    return Response({re.split(r'\x1B[\[0-9;]*m', prompt)[2]})
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def setDCVoltage(request, volts=0):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+    client.connect('hpt-k5700.colab.duke.edu', port=2222, username='admin6', password='foxY458')
+    conn = client.invoke_shell()
+
+    # TODO: Where get volts
+    conn.recv(10000)
+    conn.send('set dc ' + volts + '\n')
+
+    n = 0
+    while n < 3:
+        time.sleep(0.5)
+        if conn.recv_ready():
+            break
+
+    prompt = conn.recv(10000).decode('ascii')
+
+    client.close()
+    return Response({re.split(r'\x1B[\[0-9;]*m', prompt)[2]})
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def setACVoltage(request, volts=0, freq=0):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+    client.connect('hpt-k5700.colab.duke.edu', port=2222, username='admin6', password='foxY458')
+    conn = client.invoke_shell()
+
+    # TODO: Where get volts and frequency
+    conn.recv(10000)
+    conn.send('set ac ' + volts + ' ' + freq + '\n')
 
     n = 0
     while n < 3:
